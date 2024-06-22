9038f0d6ed7840c78b20afc72c1ee0e19309ff37
jeannette <jeannettearias@gmail.com>
2024-06-22 14:06:35 +0200

    the API is returning data

--- src/js/main.js
+++ src/js/main.js
@@ -12,14 +12,14 @@ const SearchBtn = document.querySelector('js__searchBtn');
 // DATA AND GLOBAL VARIABLES
 
     //OBJECT ARRAYS
-    
-
-
-    
+    let cards = [];  
+    let favourites = [];
 
 
 
 //FUNCTIONS AND EVENTS
+
+
 
 
 //ADD CARDS TO FAVORITES
@@ -32,12 +32,32 @@ const SearchBtn = document.querySelector('js__searchBtn');
 
     //CLICK'S EVENT - CARDS
 
+function handleClickCard(ev) {
+  ev.preventDefault();
+
+}
+
+cardsUl.addEventListener( 'click', handleClickCard );
 
 
     //CLICK'S EVENT - SEARCH
+/*
+function handleClickFavourite(ev) {
+    //code
+
+}
+
+favouritessUl.addEventListener( 'click', handleClickFavourite );
+*/
 
 
+    //FETCH AND LOCALSTORAGE 
 
-
-
+fetch ('https://api.disneyapi.dev/character?pageSize=10')     
+    .then(response => response.json())
+    .then(dataFromFetch => {
+        console.log(dataFromFetch.data);
+    
+        }
+    )
 
