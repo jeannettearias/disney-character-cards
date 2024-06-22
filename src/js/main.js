ea9965616df1542fd93706908000e73261ec4f34
jeannette <jeannettearias@gmail.com>
2024-06-22 13:30:48 +0200

    html and js structure

---
+++ src/js/main.js
@@ -0,0 +1,43 @@
+'use strict'
+
+// QUERY SELECTORS
+const cardsUl = document.querySelector('.js__cardsUl');
+const favouritessUl = document.querySelector('.js__favouritesUl');
+
+const SearchInput = document.querySelector('.js__searchInput');
+const SearchBtn = document.querySelector('js__searchBtn');
+
+
+
+// DATA AND GLOBAL VARIABLES
+
+    //OBJECT ARRAYS
+    
+
+
+    
+
+
+
+//FUNCTIONS AND EVENTS
+
+
+//ADD CARDS TO FAVORITES
+
+
+
+//REMOVE CARDS FROM FAVORITES
+
+
+
+    //CLICK'S EVENT - CARDS
+
+
+
+    //CLICK'S EVENT - SEARCH
+
+
+
+
+
+
