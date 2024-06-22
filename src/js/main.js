d467cd048d56aa7f073021fc5aa10b9c46afb368
jeannette <jeannettearias@gmail.com>
2024-06-23 01:40:47 +0200

    solve the id problem

--- src/js/main.js
+++ src/js/main.js
@@ -24,27 +24,42 @@ let favourites = [];
 
 
 //create LiCard and increment them one by one
-function renderCards(cards) {
+function createLiForCards(cards) {
 
     let cardHTML = '';
 
     for (const card of cards) {
-        cardHTML += `<li class="cards">
+        cardHTML += `<li class="js__card cards" data-id= "${card._id}">
         <img class = "imgLi" src="${card.imageUrl}" alt="Picture of ${card.name}">
         <p class = "nameLi">${card.name}</p>
       </li>`;
-    }
+    };
 
     return cardHTML;
+}
+
+function renderCards(cards) {
+    cardsUl.innerHTML = createLiForCards(cards);
+
+    const allCards = document.querySelectorAll('.js__card');
+
+    for (const eachCard of allCards) {
+        eachCard.addEventListener('click', handleClickCard);
+    }
 
 }
 
-
-
-
-
 //ADD CARDS TO FAVORITES
+function renderFavourites() {
+    let favouriteHTML = '';
 
+    for ( const favouriteCard of favourites ) {
+        favouriteHTML += createLiForCards(favouriteCard);
+    }
+
+    favouritessUl.innerHTML = favouriteHTML;
+
+}
 
 
 //REMOVE CARDS FROM FAVORITES
@@ -59,14 +74,18 @@ function renderCards(cards) {
 //CLICK'S EVENT - CARDS
 
 function handleClickCard(ev) {
-    ev.preventDefault();
+    //ev.preventDefault();
+
+    const clickedImageId = ev.currentTarget.dataset.id;
+    console.log(clickedImageId);
+
 
 }
 
-cardsUl.addEventListener('click', handleClickCard);
 
 
 //CLICK'S EVENT - SEARCH
+
 
 
 
@@ -75,12 +94,12 @@ cardsUl.addEventListener('click', handleClickCard);
 fetch('https://api.disneyapi.dev/character?pageSize=10')
     .then(response => response.json())
     .then(dataFromFetch => {
-        console.log(dataFromFetch.data);
 
         cards = dataFromFetch.data;
 
-        cardsUl.innerHTML = renderCards(cards);
+        renderCards(cards);
+
     });
 
-    console.log(cards);
+
 
